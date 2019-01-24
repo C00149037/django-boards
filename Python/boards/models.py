d1e4b8e9fc4c413f765e9620781e99cfa02d1279
James Brown <c00149037@itcarlow.ie>
2019-01-24 15:06:43 +0000

    Part 3 Commit 1

--- Python/boards/models.py
+++ Python/boards/models.py
@@ -12,8 +12,9 @@ class Board(models.Model):
 class Topic(models.Model):
     subject = models.CharField(max_length=255)
     last_updated = models.DateTimeField(auto_now_add=True)
-    board = models.ForeignKey(Board, related_name='topics', on_delete=models.CASCADE)
+    board = models.ForeignKey(Board, related_name='topics', 		on_delete=models.CASCADE)
     starter = models.ForeignKey(User, related_name='topics', on_delete=models.CASCADE)
+
 
 
 class Post(models.Model):
